1e0a1ccae7bd6051e2e43d077489f57d520ba02b
Alvaro Lopez Garcia <lopeza@alpha05.aulas.gsyc.urjc.es>
2018-11-22 14:44:26 +0100

    Modificado static

--- Bisiesto/Bisiesto.java
+++ Bisiesto/Bisiesto.java
@@ -1,9 +1,9 @@
 public class Bisiesto
 {
 	//Consideramos los anyos negativos como anyos despues de cristo
-	static public boolean EsBisiesto(int anyo){
+	public boolean EsBisiesto(int anyo){
 		if((anyo %4 == 0) && ((anyo %100 != 0) || (anyo %400 == 0))){
-			return true;	
+			return true;
 		}else{
 			return false;
 		}
--- Bisiesto/BisiestoTest.java
+++ Bisiesto/BisiestoTest.java
@@ -3,33 +3,42 @@ import org.junit.*;
 
 public class BisiestoTest
 {
+
+    private Bisiesto anyo;
+
+    @Before      // Set up - Called before every test method.
+    public void setUp()
+    {
+       anyo = new Bisiesto();
+    }
+
 	@Test public void testAnyoCero()
 	{
-		assertTrue("Es bisiesto cero", true == Bisiesto.EsBisiesto(0));
+		assertTrue("Es bisiesto cero", true == anyo.EsBisiesto(0));
 	}
 
 	@Test public void testAnyoPositivo()
 	{
-		assertTrue("Es bisiesto 1", false == Bisiesto.EsBisiesto(1));
+		assertTrue("Es bisiesto 1", false == anyo.EsBisiesto(1));
 	}
 
 	@Test public void testAnyoNegativo()
 	{
-		assertTrue("Es bisiesto -1", false == Bisiesto.EsBisiesto(-1));
+		assertTrue("Es bisiesto -1", false == anyo.EsBisiesto(-1));
 	}
 
 	@Test public void testAnyoCien()
 	{
-		assertTrue("Es bisiesto 100", false == Bisiesto.EsBisiesto(100));
+		assertTrue("Es bisiesto 100", false == anyo.EsBisiesto(100));
 	}
 
 	@Test public void testAnyoCuatro()
 	{
-		assertTrue("Es bisiesto 4", true == Bisiesto.EsBisiesto(4));
+		assertTrue("Es bisiesto 4", true == anyo.EsBisiesto(4));
 	}
 
 	@Test public void testAnyoCuatrocientos()
 	{
-		assertTrue("Es bisiesto 400", true == Bisiesto.EsBisiesto(400));
+		assertTrue("Es bisiesto 400", true == anyo.EsBisiesto(400));
 	}
 }
