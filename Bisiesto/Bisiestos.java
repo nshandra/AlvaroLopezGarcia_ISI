98fbe91d7844216108c986a306666d327aa86e69
jaimefdez96 <jfs_x@hotmail.com>
2018-11-26 17:52:36 +0100

    Bisiestos modificado, probado con mi test.

--- Bisiesto/Bisiestos.java
+++ Bisiesto/Bisiestos.java
@@ -1,7 +1,11 @@
 public class Bisiestos
 {
 	//Consideramos los anyos negativos como anyos despues de cristo
+	//---> He cambiado EsBisiesto por esBisiesto
 	public boolean esBisiesto(int anyo){
+		//---> Añadida gestión de números negativos
+		if(anyo < 0) throw new IllegalArgumentException();
+
 		if((anyo %4 == 0) && ((anyo %100 != 0) || (anyo %400 == 0))){
 			return true;
 		}else{
